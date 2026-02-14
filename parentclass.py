08ab20b3452085e0706b7c8ef1beec19a08b1dc3
palashwika <paladhiashwika@gmail.com>
2026-02-14 08:35:25 -0800

    Inheritance

---
+++ parentclass.py
@@ -0,0 +1,30 @@
+#Python code to demonstrate parent constructors
+#are called
+
+#parent class
+class Person(object):
+
+    # __init__ is known as the constructor
+
+
+    def __init__(self,name,idnumber):
+        self.name=name
+        self.idnumber=idnumber
+    def display(self):
+        print(self.name)
+        print(self.idnumber)
+
+#child Class
+class Employee(Person):
+    def __init__(self,name,idnumber,salary,post):
+        self.salary=salary
+        self.post=post
+
+        #invoking the __init__ of the parent class
+        Person.__init__(self,name,idnumber)
+
+#creation of an object variable or an instance
+a=Employee('Sara', 843290, 30002, "Manager")
+
+#calling a function of the class Person using its instance
+a.display()
